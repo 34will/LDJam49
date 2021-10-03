a9a5ad621540eca6ae11c7abe3f85953f025cbbf
34will <wgilmour@hotmail.co.uk>
2021-10-03 16:02:11 +0100

    Added NonOwnerColliderDisable to hopefully help with lag. Added NonOwnerColliderDisable to SquarePlatform.

---
+++ Assets/Scripts/NonOwnerColliderDisable.cs
@@ -0,0 +1,18 @@
+using Photon.Pun;
+using UnityEngine;
+
+namespace Unstable
+{
+    public class NonOwnerColliderDisable : MonoBehaviour
+    {
+        public void Start()
+        {
+            if (PhotonNetwork.IsMasterClient)
+                return;
+
+            Collider[] colliders = GetComponentsInChildren<Collider>();
+            foreach (Collider collider in colliders)
+                collider.enabled = false;
+        }
+    }
+}
