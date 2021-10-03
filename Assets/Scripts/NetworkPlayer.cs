c681a201ae1a8e85261924e589847a36b1c2613e
34will <wgilmour@hotmail.co.uk>
2021-10-03 17:20:55 +0100

    Added rudimentary player death

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -1,13 +1,25 @@
+using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 
 namespace Unstable
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class NetworkPlayer : MonoBehaviourPun
     {
         public static GameObject LocalPlayerInstance;
 
+        private Rigidbody rigidBody;
+
         public GameObject Camera;
+
+        public GameObject Model;
+
+        public bool IsDead = false;
+
+        public float DeathHeight = 2.0f;
+
+        public List<MonoBehaviour> ToDisableOnDeath;
 
         public void Awake()
         {
@@ -20,6 +32,31 @@ namespace Unstable
         public void Start()
         {
             Camera.SetActive(photonView.IsMine);
+
+            if (!PhotonNetwork.IsMasterClient)
+                return;
+
+            rigidBody = GetComponent<Rigidbody>();
+        }
+
+        public void Update()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+                return;
+
+            if (transform.position.y > DeathHeight || IsDead)
+                return;
+
+            rigidBody.position = new Vector3(0.0f, 30.0f, 0.0f);
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.angularVelocity = Vector3.zero;
+
+            Destroy(Model);
+
+            foreach (MonoBehaviour behaviour in ToDisableOnDeath)
+                behaviour.enabled = false;
+            
+            IsDead = true;
         }
     }
 }
